5097e93c998ae7ff662c956bfde67c4126b2b7b5
laifeipeng <1020103647@qq.com>
2021-03-16 21:23:00 +0800

    feat: 首页demo

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import './plugins/axios'
+import './scss/index.scss'
 
 Vue.config.productionTip = false
 
@@ -11,5 +12,5 @@ new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app')
