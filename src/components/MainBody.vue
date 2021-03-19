4c2dcfa36c06031bb1f8981d6f998ae64e9c009c
guodayang999 <33561649+guodayang999@users.noreply.github.com>
2021-03-19 17:26:32 +0800

    mainBody 50%

--- src/components/MainBody.vue
+++ src/components/MainBody.vue
@@ -1,142 +1,188 @@
+
+<style>
+    .container{
+        position: relative;
+        width: 100%;
+       
+    }
+    .container input{
+        width: 100%;
+        height: 30px;
+		opacity: 0.16;
+		border-radius: 24px;
+		background: #D8D8D8;
+    }
+    .container button{
+		width: 25%;
+		 height: 30px;
+        position: absolute;
+        right: 0;
+        background-color: #4C4C4C;
+		border-radius: 24px;
+        border: none;   
+        outline: none;  
+     }
+</style>
+
 <template>
-  <v-container>
+
     <v-row class="text-center">
+		
+		<v-col class="mb-12">
+			 <div style="height: 45px;font-size: 32px;font-family: Nunito-Regular, Nunito;font-weight: 400;color: #000000;line-height: 43px;">
+				企业动态
+			 </div>
+		</v-col>
+		
       <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
+      <div style=" width: 100%;height: 340px;background: linear-gradient(92deg, #00CFAC 0%, #5B7ADE 100%);"></div>
       </v-col>
 
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify1111111
-        </h1>
+     <v-col class="mb-12">
+		 <div style="height: 45px;font-size: 32px;font-family: Nunito-Regular, Nunito;font-weight: 400;color: #000000;line-height: 43px;">
+			投资机构 
+		 </div>
+	</v-col>
+	
+	<v-col cols="12" style="height: 336px;">
+		<v-row>
+		<v-col cols="3"></v-col>
+		<v-col cols="6" >
+			<v-row style="height: 150px;">
+				<v-col cols="3" ><img src="../assets/hezuo_logo1.png" style="width:100%;height:auto;"/></v-col>
+				<v-col cols="3" ><img src="../assets/hezuo_logo2.png" style="width:100%;height:auto;"/></v-col>
+				<v-col cols="3" ><img src="../assets/hezuo_logo1.png" style="width:100%;height:auto;"/></v-col>
+				<v-col cols="3" ><img src="../assets/hezuo_logo2.png" style="width:100%;height:auto;"/></v-col>
+			</v-row>
+			<v-row  style="height: 150px;">
+				<v-col cols="3" ><img src="../assets/hezuo_logo2.png" style="width:100%;height:auto;"/></v-col>
+				<v-col cols="3" ><img src="../assets/hezuo_logo1.png" style="width:100%;height:auto;"/></v-col>
+				<v-col cols="3" ><img src="../assets/hezuo_logo2.png" style="width:100%;height:auto;"/></v-col>
+				<v-col cols="3" ><img src="../assets/hezuo_logo1.png" style="width:100%;height:auto;"/></v-col>
+			</v-row>
+		</v-col>		
+		<v-col cols="3" ></v-col>
+		</v-row>
+	</v-col>
+	
+	<v-col class="mb-12">
+		 <div style="width: 100%;height: 45px;font-size: 32px;font-family: PingFang-SC-Semibold, PingFang-SC;font-weight: 600;line-height: 45px;">
+			规划路线
+		 </div>
+	</v-col>
+	
+	<v-col cols="12" style="height: 336px;">
+		<v-row>
+		<v-col cols="3"></v-col>
+		<v-col cols="6" style="height: 748px;background: ivory;" >
+			
+		</v-col>		
+		<v-col cols="3" ></v-col>
+		</v-row>
+	</v-col>
 
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br />please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank"
-            >Discord Community</a
-          >
-        </p>
-      </v-col>
+ <v-col cols="12" style="height: 137px;background: black;">
+</v-col>	 
+ <v-col cols="12" style="height: 350px;background: black;">
+			
+		<v-row>
+			<v-col cols="3" style="background: blue;">col-20</v-col>
+		  <v-col cols="5" style="background: black;">
+			  <table>
+			    <tr>
+				  <th align="left" valign="top" style="width: 178px;height: 45px;font-size: 18px;font-family: Nunito-SemiBold, Nunito;font-weight: 600;color: #FFFFFF;line-height: 16px;">
+					  公司
+					  </th>
+			      <th align="left" valign="top" style="width: 178px;height: 45px;font-size: 18px;font-family: Nunito-SemiBold, Nunito;font-weight: 600;color: #FFFFFF;line-height: 16px;">
+					  产品</th>
+				  <th align="left" valign="top" style="width: 178px;height: 45px;font-size: 18px;font-family: Nunito-SemiBold, Nunito;font-weight: 600;color: #FFFFFF;line-height: 16px;">
+					  帮助</th>
+			    </tr>
+				<tr>			      
+				<td align="left" valign="top" style="width: 158px;height: 35px;font-size: 14px;font-family: Nunito-Regular, Nunito;font-weight: 400;color: #FFFFFF;line-height: 14px;">
+					关于我们</td>
+			      <td align="left" valign="top" style="width: 158px;height: 35px;font-size: 14px;font-family: Nunito-Regular, Nunito;font-weight: 400;color: #FFFFFF;line-height: 14px;">
+					  APP 钱包</td>
+				  <td align="left" valign="top" style="width: 158px;height: 35px;font-size: 14px;font-family: Nunito-Regular, Nunito;font-weight: 400;color: #FFFFFF;line-height: 14px;">
+					  帮助中心</td>
+			    </tr>
+				<tr>				  
+				  <td align="left" valign="top" style="width: 158px;height: 35px;font-size: 14px;font-family: Nunito-Regular, Nunito;font-weight: 400;color: #FFFFFF;line-height: 14px;">
+					  安全与合规</td>
+				  <td align="left" valign="top" style="width: 158px;height: 35px;font-size: 14px;font-family: Nunito-Regular, Nunito;font-weight: 400;color: #FFFFFF;line-height: 14px;">
+					  HSF代币</td>
+				  <td align="left" valign="top" style="width: 158px;height: 35px;font-size: 14px;font-family: Nunito-Regular, Nunito;font-weight: 400;color: #FFFFFF;line-height: 14px;">
+					  建议与反馈</td>
+				</tr>
+				<tr>
+				 <td align="left" valign="top" style="width: 158px;height: 35px;font-size: 14px;font-family: Nunito-Regular, Nunito;font-weight: 400;color: #FFFFFF;line-height: 14px;">
+					 联系我们</td>
+				  <td align="left" valign="top" style="width: 158px;height: 35px;font-size: 14px;font-family: Nunito-Regular, Nunito;font-weight: 400;color: #FFFFFF;line-height: 14px;">
+					  理财</td>
+				  <td align="left" valign="top" style="width: 158px;height: 35px;font-size: 14px;font-family: Nunito-Regular, Nunito;font-weight: 400;color: #FFFFFF;line-height: 14px;">
+					  提交工单</td>
+				</tr>
+				<tr>
+				  <td align="left" valign="top" style="width: 158px;height: 35px;font-size: 14px;font-family: Nunito-Regular, Nunito;font-weight: 400;color: #FFFFFF;line-height: 14px;">
+					  白皮书</td>
+				  <td align="left" valign="top" style="width: 158px;height: 35px;font-size: 14px;font-family: Nunito-Regular, Nunito;font-weight: 400;color: #FFFFFF;line-height: 14px;">
+					  社区</td>
+				  <td></td>
+				</tr>
+				<tr>
+			<td align="left" valign="top" style="width: 158px;height: 35px;font-size: 14px;font-family: Nunito-Regular, Nunito;font-weight: 400;color: #FFFFFF;line-height: 14px;">
+				企业动态</td>
+				  <td></td>
+				  <td></td>
+				</tr>
+			  </table>
+			  
+		  </v-col>
+		  <v-col cols="4" style="background: cadetblue;">
+			  <!-- <div class="container" style="width: 100%; position: relative;">
+				  <input type="text" placeholder="   nane@email.com" style="background: #D8D8D8;border-radius: 24px;opacity: 0.16;">
+				  <button style="width: 20%;background: #4C4C4C;border-radius: 24px;">注册</button>
+			  </div> -->
+		
+		<v-col cols="12" >
+			<div class="container">
+			        <input type="text" placeholder="  nane@email.com">
+			        <button>注册</button>
+			</div>
+		</v-col> 
+		<v-row>	  
+			<v-col cols="7" >
+				<div class="container">
+				        <input type="text" placeholder="  nane@email.com">
+				        <button>注册</button>
+				</div>
+			</v-col>
+			<v-col cols="5" style="background: chartreuse;" ></v-col>
+		</v-row>	  	  
+		    </v-col>
+		</v-row>
+ 
+  </v-col>
+  
+  <v-col cols="12" style="height: 144px;background: black;">
+  			
+  		<v-row>
+  			<v-col cols="3" style="background: blue;">col-20</v-col>
+  		  <v-col cols="5" style="background: brown;">
+  			  
+  		  </v-col>
+  		  <v-col cols="4" style="background: blueviolet;">col-4</v-col>
+  		</v-row>
+  
+   </v-col>
+ 
 
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
     </v-row>
-  </v-container>
+ 
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-
-  data: () => ({
-    ecosystem: [
-      {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader',
-      },
-      {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify',
-      },
-      {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify',
-      },
-    ],
-    importantLinks: [
-      {
-        text: 'Documentation',
-        href: 'https://vuetifyjs.com',
-      },
-      {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com',
-      },
-      {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify',
-      },
-      {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs',
-      },
-      {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify',
-      },
-    ],
-    whatsNext: [
-      {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com/components/api-explorer',
-      },
-      {
-        text: 'Select a layout',
-        href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
-      },
-      {
-        text: 'Frequently Asked Questions',
-        href:
-          'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-      },
-    ],
-  }),
 }
 </script>
