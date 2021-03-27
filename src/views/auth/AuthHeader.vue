c5e619dfd18a3d7dbf1f3ce37f56a6eb466f4103
pegion <1020103647@qq.com>
2021-03-27 19:18:13 +0800

    refactor: AuthHeader

--- src/views/auth/AuthHeader.vue
+++ src/views/auth/AuthHeader.vue
@@ -1,9 +1,20 @@
 <template>
   <v-container style="padding: 20px;">
-    <v-row>
-      <v-col cols="2" style="height: 20px; "
-        ><img src="@/assets/logo_black.png" style="width:126px;height:auto;"
-      /></v-col>
+    <div class="d-flex align-center justify-space-between">
+      <img src="@/assets/logo_black.png" style="width:126px;height:30px;" />
+      <div class="d-flex flex-column">
+        <img src="@/assets/h1.png" style="width:126px;height:auto;" />
+        &#32;
+        <img
+          src="@/assets/h2.png"
+          style="width:126px;height:auto; margin-top: 10px"
+        />
+      </div>
+    </div>
+    <!-- <v-row>
+      <v-col cols="2" style="height: 20px; ">
+        <img src="@/assets/logo_black.png" style="width:126px;height:auto;" />
+      </v-col>
       <v-col cols="2" style="height: 20px;"> </v-col>
       <v-col cols="2" style="height: 20px; "></v-col>
       <v-col cols="4" style="height: 20px; "></v-col>
@@ -12,8 +23,7 @@
         &#32;
         <img src="@/assets/h2.png" style="width:126px;height:auto;" />
       </v-col>
-      <!-- <v-col cols="2" style="height: 20px; ">xxx</v-col> -->
-    </v-row>
+    </v-row> -->
   </v-container>
 </template>
 
@@ -21,7 +31,7 @@
 export default {
   name: 'AuthHeader',
   components: {},
-  data() {
+  data () {
     return {}
   },
 }
