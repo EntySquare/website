1e520417947a7ef00d92a75eadc54550d48059a8
pegion <1020103647@qq.com>
2021-03-27 19:27:50 +0800

    refactor: login.vue

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,21 +1,16 @@
 <template>
-  <v-app style="background: linear-gradient(136deg, #FFFFFF 0%, #F3F6FA 100%);">
-    <!--应用程序导航条-->
-    <v-col cols="12">
+  <div style="background: linear-gradient(136deg, #FFFFFF 0%, #F3F6FA 100%);">
+    <v-container>
       <AuthHeader />
-    </v-col>
-    <v-row dense="true"></v-row>
-    <!--左侧菜单导航-->
-    <v-row>
-      <v-col cols="2"> </v-col>
-      <v-col cols="5">
+      <div
+        class="d-flex justify-space-between mt-8"
+        style="max-width: 80%; margin:auto"
+      >
         <AuthLeft />
-      </v-col>
-      <v-col cols="3">
         <LoginMod />
-      </v-col>
-    </v-row>
-  </v-app>
+      </div>
+    </v-container>
+  </div>
 </template>
 
 <script>
